bf1df7a740c54fc19274788e3a19372390d9b593
Andy Galkin <andy.galkin@gmail.com>
2022-07-13 16:11:44 -0700

    dactyl: added caps_word
    preonic: moved caps_word, added del and insert main layer

--- keyboards/preonic/keymaps/galkk/keymap.c
+++ keyboards/preonic/keymaps/galkk/keymap.c
@@ -2,11 +2,11 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_ortho_5x12(
-	KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, RGUI(KC_SPC),
+	CAPS_WORD, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, RGUI(KC_SPC),
 	KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, SGUI_T(KC_P), RCS(KC_NO),
 	KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, RGUI_T(KC_SCLN), MT(MOD_LCTL | MOD_RGUI, KC_QUOTE),
-	CAPS_WORD, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, LCA_T(KC_SLSH), RSA(KC_NO),
-	KC_NO, KC_LALT, KC_LCTL, MO(2), KC_LSFT, KC_BSPC, KC_ENT, KC_SPC, MO(1), KC_LBRC, KC_RBRC, KC_NO),
+	KC_INS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, LCA_T(KC_SLSH), RSA(KC_NO),
+	KC_DEL, KC_LALT, KC_LCTL, MO(2), KC_LSFT, KC_BSPC, KC_ENT, KC_SPC, MO(1), KC_LBRC, KC_RBRC, KC_NO),
 
 [1] = LAYOUT_ortho_5x12(
 	KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_BTN1, KC_BTN2, KC_NO, KC_NO, KC_NO, KC_NO)
 };
 
-// RGB lightning documentation: 
+// RGB lightning documentation:
 // https://github.com/qmk/qmk_firmware/blob/master/docs/feature_rgblight.md
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
