e1157bdde13e08224836e65dfa9d5af871cde3ea
Andy Galkin <andy.galkin@gmail.com>
2022-08-21 23:35:28 -0700

    additional changes

--- keyboards/preonic/keymaps/galkk/keymap.c
+++ keyboards/preonic/keymaps/galkk/keymap.c
@@ -6,7 +6,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,    KC_W,    KC_E,  KC_R,    KC_T,    KC_Y,   KC_U,   KC_I,             KC_O,    SGUI_T(KC_P),    RCS(KC_NO),
     KC_ESC,    KC_A,    KC_S,    KC_D,  KC_F,    KC_G,    KC_H,   KC_J,   KC_K,             KC_L,    RGUI_T(KC_SCLN), MT(MOD_LCTL | MOD_RGUI, KC_QUOTE),
     KC_INS,    KC_Z,    KC_X,    KC_C,  KC_V,    KC_B,    KC_N,   KC_M,   KC_COMM,          KC_DOT,  LCA_T(KC_SLSH),  RSA(KC_NO),
-    KC_DEL,    KC_LALT, KC_LCTL, MO(2), KC_LSFT, KC_BSPC, KC_ENT, KC_SPC, LT(1, KC_SPC),    KC_LBRC, KC_RBRC,         KC_NO),
+    KC_DEL,    KC_LALT, KC_LCTL, MO(2), KC_LSFT, KC_BSPC, KC_ENT, KC_ENT, LT(1, KC_SPC),    KC_LBRC, KC_RBRC,         KC_NO),
 
 [1] = LAYOUT_ortho_5x12(
     KC_F1, KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
