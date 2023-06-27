cd1b1b674fc7514929495078b7cbb874a42ccdff
Andy Galkin <andy.galkin@gmail.com>
2023-06-27 14:10:49 -0700

    constants for layer numbers and some moved characters according to recent moonlander layout changes

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/galkk/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/galkk/keymap.c
@@ -1,5 +1,10 @@
 #include QMK_KEYBOARD_H
 
+#define BASE_LAYER          0
+#define CHARACTERS          1
+#define KEY_COMBINATIONS    2
+#define CURSOR              3
+
 enum custom_keycodes {
     POINTER_DEREFERENCE = SAFE_RANGE,
 };
@@ -21,7 +26,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #define RIGHT_THUMB_BLOCK(p1,p2,p3,p4,p5,p6)p3, p6, p2, p5, p3, p4
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT_5x7(
+	[BASE_LAYER] = LAYOUT_5x7(
         KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5, KC_NO,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_NO,
         KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_NO,
@@ -29,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_NO, KC_NO, QK_LOCK,
 
     // lower upper, left -> right
-            KC_LSFT, MO(3),
+            KC_LSFT, MO(CURSOR),
             KC_BSPC, KC_LCTL,
             KC_ENT, KC_NO,
 
@@ -37,16 +42,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_Y, KC_U, KC_I, KC_O, SGUI_T(KC_P), RCS(KC_NO),
         KC_NO, KC_H, KC_J, KC_K, KC_L, RGUI_T(KC_SCLN), MT(MOD_LCTL | MOD_RGUI, KC_QUOTE),
         KC_N, KC_M, KC_COMM, KC_DOT, LCA_T(KC_SLSH), RSA(KC_NO),
-        MO(2), KC_LBRC, KC_RBRC, MEH(KC_NO),
+        MO(KEY_COMBINATIONS), KC_LBRC, KC_RBRC, MEH(KC_NO),
 
     // upper lower, right -> left
-            MO(1), KC_SPC,
+            MO(CHARACTERS), KC_SPC,
             KC_LGUI, KC_RSFT,
             KC_NO, KC_RALT),
 
-	[1] = LAYOUT_5x7(
+	[CHARACTERS] = LAYOUT_5x7(
         KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_BSLS, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_CIRC, KC_BSLS, KC_NO, KC_NO,
         KC_NO, KC_DLR, KC_PLUS, KC_MINS, KC_ASTR, KC_NO, KC_NO,
         KC_NO, KC_AT, KC_COLN, KC_HASH, KC_PERC, KC_NO,
         KC_NO, KC_NO, KC_INSERT, KC_DELETE,
@@ -56,16 +61,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_NO,
 
         KC_NO, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
-        KC_NO, KC_NO, KC_UNDS, KC_DQUO, KC_QUOT, KC_CIRC, KC_F12,
-        KC_NO, KC_NO, KC_AMPR, KC_PIPE, KC_EXLM, KC_EQL, KC_NO,
-        KC_NO, KC_TILD, KC_GRV, KC_NO, KC_NO, POINTER_DEREFERENCE,
+        KC_NO, KC_NO, KC_UNDS, KC_QUOT, KC_DQUO, KC_CIRC, KC_F12,
+        KC_NO, KC_TILD, KC_AMPR, KC_PIPE, KC_EXLM, KC_EQL, KC_NO,
+        KC_NO, KC_GRV, KC_NO, KC_NO, KC_NO, POINTER_DEREFERENCE,
         KC_NO, KC_LPRN, KC_RPRN, KC_NO,
 
         KC_NO, KC_TRNS,
         KC_NO, KC_NO,
         KC_NO, KC_NO),
 
-	[2] = LAYOUT_5x7(
+	[KEY_COMBINATIONS] = LAYOUT_5x7(
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
@@ -84,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_NO,
         KC_NO, KC_NO),
 
-	[3] = LAYOUT_5x7(
+	[CURSOR] = LAYOUT_5x7(
         KC_NO, LCTL(KC_HOME), LCTL(KC_PGDN), LCTL(KC_PGUP), LCTL(KC_END), KC_NO, KC_NO,
         KC_NO, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, LCTL(KC_F4),
         KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, LCTL(KC_BSPC), KC_NO,
