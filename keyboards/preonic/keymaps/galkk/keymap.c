f84bc67f5302b38476529dd49969007bf5b66e98
Andy Galkin <andy.galkin@gmail.com>
2023-08-03 22:39:48 -0700

    Trying to set polling rate for preonic for 1khz

--- keyboards/preonic/keymaps/galkk/keymap.c
+++ keyboards/preonic/keymaps/galkk/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, _______, _______,
     KC_CAPS_LOCK, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, _______, KC_TILD,
     KC_LALT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP, _______,
-    TG(BASE_LAYER), KC_LCTL, KC_SPC, KC_LEFT, KC_LSFT, KC_BSPC, KC_ENT, _______, KC_SPC, KC_LEFT, KC_DOWN, KC_RGHT),
+    _______, KC_LCTL, KC_SPC, KC_LEFT, KC_LSFT, KC_BSPC, KC_ENT, _______, KC_SPC, KC_LEFT, KC_DOWN, KC_RGHT),
 
 [CHARACTERS] = LAYOUT_ortho_5x12(
     KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
